4b4057fdcf1e7f2e975eb46ad303838628c9f322
d-coder <82116626+d-realcoder@users.noreply.github.com>
2021-09-04 17:05:32 +0530

    Create Reducers for client

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,1 +1,12 @@
+import { combineReducers } from 'redux';
+import auth from './auth';
+import interviewAnalysis from './interviewAnalysis';
+import pitchAnalysis from './pitchAnalysis';
+import dashboard from './dashboard'
 
+export default combineReducers({
+    auth,
+    interviewAnalysis,
+    pitchAnalysis,
+    dashboard,
+});
