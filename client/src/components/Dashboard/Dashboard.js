82ef6f298cb107ef715000b216ef64455c4a2003
codebosss <88897108+codebosss@users.noreply.github.com>
2021-09-05 00:07:56 +0530

    minor changes

--- client/src/components/Dashboard/Dashboard.js
+++ client/src/components/Dashboard/Dashboard.js
@@ -22,7 +22,7 @@ function Copyright() {
         <Typography variant="body2" color="textSecondary" align="center">
             {'Copyright © '}
             <Link color="inherit" href="https://material-ui.com/">
-                Vishleshan
+                Analyza
             </Link>{' '}
             {new Date().getFullYear()}
             {'.'}
