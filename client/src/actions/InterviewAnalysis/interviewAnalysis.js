c928efb526121ccc110726bcd49c0821a5c7e983
d-coder <82116626+d-realcoder@users.noreply.github.com>
2021-09-04 17:33:59 +0530

    Add actions for Interview Analysis

--- client/src/actions/InterviewAnalysis/interviewAnalysis.js
+++ client/src/actions/InterviewAnalysis/interviewAnalysis.js
@@ -14,3 +14,27 @@ export const joinZoomCall = (formData, history) => async (dispatch) => {
         console.log(error);
     }
 }
+
+export const getAnalysis = (formData) => async (dispatch) => {
+    try {
+        console.log(formData)
+        const { data } = await api.getAnalysis(formData)
+
+        dispatch({ type: GET_INTERVIEW_ANALYSIS, data })
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export const stopAnalysis = (connectionId) => async (dispatch) => {
+    try {
+        const { data } = await api.stopAnalysis(connectionId)
+        console.log(data)
+        dispatch({ type: STOP_ANALYSIS, data })
+
+
+
+    } catch (error) {
+        console.log(error);
+    }
+}
